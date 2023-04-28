841c304a20daccaab61fac9a0aa5e594c65db174
Gauravbeh <gauravbehere58@gmail.com>
2023-04-28 18:45:35 +0530

    i changed many blogs

--- js/script.js
+++ js/script.js
@@ -48,14 +48,15 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web-Developer","Designer", "Android-Developer"],
+        strings: [ "Web-Developer","Android-Developer","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
+
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer","Designer", "Tester"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
